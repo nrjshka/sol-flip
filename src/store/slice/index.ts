1363ff99270febd3a43cf8a77c3f778b21449b33
Max Korolev <nrjshka@gmail.com>
2022-04-19 20:20:45 +0300

    redux: added app store

--- src/store/slice/index.ts
+++ src/store/slice/index.ts
@@ -1,1 +1,16 @@
+import { combineReducers } from '@reduxjs/toolkit';
+
+import app, { AppState } from './app';
+
+const reducer = combineReducers({
+  app,
+})
+
+type StoreType = {
+  app: AppState
+}
+
+export { reducer };
+export type { StoreType };
+
 export * from './app'
